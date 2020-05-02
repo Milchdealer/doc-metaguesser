b7d64d651e14941ba017d5a6d239036588b23d29
Jannik F <jocker300@hotmail.de>
2020-05-02 23:38:51 +0200

    Syntax typos

--- txt-metaguesser/src/db.py
+++ txt-metaguesser/src/db.py
@@ -7,7 +7,6 @@ from datetime import datetime
 
 from sqlalchemy import (
 	create_engine,
-	Session,
 	Integer,
 	String,
 	Binary,
@@ -15,6 +14,7 @@ from sqlalchemy import (
 	DateTime,
 	ForeignKey
 )
+from sqlalchemy.orm import Session
 from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.ext.declarative import declarative_base
  
@@ -23,8 +23,8 @@ Base = declarative_base()
 class DocumentStore(Base):
 	__tablename__ = "document_store"
 	filename = Column(String(1024), primary_key=True)
-	content = Column(LONGTEXT(1000000), nullable=False)
-	page_num = Column(Integer)
+	content = Column(LONGTEXT(), nullable=False)
+	page_num = Column(Integer())
 	checksum = Column(Binary(40))
 	created_on = Column(DateTime(), default=datetime.now)
 
@@ -32,8 +32,8 @@ class MetadataStore(Base):
 	__tablename__ = "document_metadata"
 	id = Column(Integer, primary_key=True)
 	filename = Column(String(1024), ForeignKey("document_store.filename"))
-	metadata_label = COLUMN(STRING(1024), nullable=False)
-	metadata_value = COLUMN(STRING(1024))
+	metadata_label = Column(String(1024), nullable=False)
+	metadata_value = Column(String(1024))
 	created_on = Column(DateTime(), default=datetime.now)
 	updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
 
@@ -43,20 +43,20 @@ def create_relations(engine, checkfirst=True):
 
 def make_connection():
 	""" Returns the engine. """
-	sqlalchemy_uri = os.getenv(SQL_ALCHEMY__URI)
+	sqlalchemy_uri = os.getenv("SQL_ALCHEMY__URI")
 	if not sqlalchemy_uri:
 		# Try with SQL_ALCHEMY__<params>, might be set by k8s or docker-swarm
 		sql_credentials = {
-			"user": os.getenv(SQL_ALCHEMY__USER, "root"),
-			"password": os.getenv(SQL_ALCHEMY__PASSWORD),
-			"host": os.getenv(SQL_ALCHEMY__HOST, "localhost"),
-			"database": os.getenv(SQL_ALCHEMY__DATABASE, "documents")
+			"user": os.getenv("SQL_ALCHEMY__USER", "root"),
+			"password": os.getenv("SQL_ALCHEMY__PASSWORD"),
+			"host": os.getenv("SQL_ALCHEMY__HOST", "localhost"),
+			"database": os.getenv("SQL_ALCHEMY__DATABASE", "documents")
 		}
 		sqlalchemy_uri = "mysql+pymysql://%s:%s@%s/%s" % (
 			sql_credentials["user"],
-			sqlalchemy["password"],
-			sqlalchemy["host"],
-			sqlalchemy["database"]
+			sql_credentials["password"],
+			sql_credentials["host"],
+			sql_credentials["database"]
 		)
 		del sql_credentials
 	engine = create_engine(sqlalchemy_uri)
