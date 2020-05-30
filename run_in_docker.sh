47dc1813eb29b92e84f2672f52bd85499b43071b
milchdealer <franzj94@googlemail.com>
2020-05-30 22:14:26 +0200

    Verbosity

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -8,10 +8,12 @@ fi
 
 # Variables
 TMP_DIR=/tmp/`date +%F"-"%H%M`
+echo "Using temporary directory at $TMP_DIR"
 
 # Build the docker images
+echo "Building docker image pdf2txt with context ./pdf-to-txt"
 docker build -t pdf2txt ./pdf-to-txt
-
+echo "Building docker image txt-metaguesser with context ./txt-metaguesser"
 docker build -t txt-metaguesser ./txt-metaguesser
 
 # Parse all input directories
@@ -19,14 +21,18 @@ for INPUT_DIR in "$@"
 do
 	TMP_OUT_DIR=$TMP_DIR/$INPUT_DIR
 	mkdir -p $TMP_OUT_DIR
+	echo "Created temporary directory $TMP_OUT_DIR"
 
+	echo "Running pdf2txt in docker with ${INPUT_DIR} as input directory"
 	docker run -v ${INPUT_DIR}:/input:ro -v ${TMP_OUT_DIR}:/output pdf2txt
-
+	echo "Running txt-metaguesser in docker on the result of ${INPUT_DIR}"
 	docker run -v ${TMP_OUT_DIR}:/input:ro \
 		-e TXT_METAGUESSER__SQL_ALCHEMY__URI=${TXT_METAGUESSER__SQL_ALCHEMY__URI} \
 		txt-metaguesser
 
 	rm -rf $TMP_OUT_DIR
+	echo "Removed temporary directory $TMP_OUT_DIR"
 done
 
 rm -r $TMP_DIR
+echo "Removed temporary directory $TMP_DIR"
