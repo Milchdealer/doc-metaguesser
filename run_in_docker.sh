39acddd20f49024835ae61c548f5c4de5027ff41
milchdealer <franzj94@googlemail.com>
2020-05-30 22:08:12 +0200

    change docker image name

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -10,7 +10,7 @@ fi
 TMP_DIR=/tmp/`date +%F"-"%H%M`
 
 # Build the docker images
-docker build -t pdf-to-txt ./pdf-to-text
+docker build -t pdf2txt ./pdf-to-text
 docker build -t txt-metaguesser ./txt-metaguesser
 
 # Parse all input directories
@@ -19,7 +19,7 @@ do
 	TMP_OUT_DIR=$TMP_DIR/$INPUT_DIR
 	mkdir -p $TMP_OUT_DIR
 
-	docker run -v ${INPUT_DIR}:/input:ro -v ${TMP_OUT_DIR}:/output pdf-to-txt
+	docker run -v ${INPUT_DIR}:/input:ro -v ${TMP_OUT_DIR}:/output pdf2txt
 	docker run -v ${TMP_OUT_DIR}:/input:ro \
 		-e TXT_METAGUESSER__SQL_ALCHEMY__URI=${TXT_METAGUESSER__SQL_ALCHEMY__URI} \
 		txt-metaguesser
